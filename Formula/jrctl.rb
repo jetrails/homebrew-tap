68675be0f2febad8a2f9dee796f8b9c2e230cc3d
Rafael Grigorian <me@raffi.io>
2020-06-23 14:43:44 -0500

    Released jrctl@1.0.1

--- Formula/jrctl.rb
+++ Formula/jrctl.rb
@@ -2,9 +2,9 @@ class Jrctl < Formula
 
 	desc "Command line tool to help interact with our API"
 	homepage "https://github.com/jetrails/jrctl"
-	url "https://github.com/jetrails/jrctl/releases/download/1.0.0/jrctl_darwin_amd64"
-	sha256 "651146f4d7bba70335bf8305f1dfd7f86d0d0dbdc0b80abbce49ee96b6715346"
-	version "1.0.0"
+	url "https://github.com/jetrails/jrctl/releases/download/1.0.1/jrctl_darwin_amd64"
+	sha256 "fedc74da9622b9cd3d2d9d86e29cdecad5588c1c9a7299ea556180c5f9ee3b82"
+	version "1.0.1"
 
 	def install
 		bin.install "jrctl_darwin_amd64" => "jrctl"
