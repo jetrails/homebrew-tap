349609d01c6fb303d282437dac4d6c8db9ae5ad2
Rafael Grigorian <me@raffi.io>
2020-07-06 12:04:51 -0500

    Released magerepo@2.0.2

--- Formula/magerepo.rb
+++ Formula/magerepo.rb
@@ -2,14 +2,16 @@ class Magerepo < Formula
 
 	desc "Cross-platform CLI tool to interact with the MageRepo API"
 	homepage "https://github.com/magerepo/magerepo-cli"
-	url "https://github.com/magerepo/magerepo-cli/releases/download/2.0.1/magerepo-cli-macos"
-	sha256 "f0784622581b42971148bef19603930bc58ae5767d4d926b10e408b42cc5cf35"
-	version "2.0.1"
-	depends_on "curl"
+	url "https://github.com/magerepo/magerepo-cli/releases/download/2.0.2/magerepo-cli-macos"
+	sha256 "9e4e88cf93c77b9cb126b48c2a5a313ad8525d6fb97baad72b3112d10c23d1da"
+	version "2.0.2"
 
 	def install
-		bin.install "magerepo-cli-macos"
-		mv bin/"magerepo-cli-macos", bin/"magerepo"
+		bin.install "magerepo-cli-macos" => "magerepo"
+	end
+
+	test do
+		system "#{bin}/magerepo", "-v"
 	end
 
 end
