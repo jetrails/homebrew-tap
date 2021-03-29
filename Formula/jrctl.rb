4ac5bc72178b9cd01f20ec784ee89228c914f950
Rafael Grigorian <me@raffi.io>
2021-03-29 10:59:27 -0500

    Released jrctl@1.2.0

--- Formula/jrctl.rb
+++ Formula/jrctl.rb
@@ -1,15 +1,12 @@
-require "tmpdir"
-
 class Jrctl < Formula
 
 	desc "Command line tool to help interact with our API"
 	homepage "https://github.com/jetrails/jrctl"
-	url "https://github.com/jetrails/jrctl/releases/download/1.1.0/jrctl-1.1.0-darwin.tar.gz"
-	sha256 "a2f50af17c64fc8470f2e74bb289214fdb154a5e128def759925c06d78163245"
-	version "1.1.0"
+	url "https://github.com/jetrails/jrctl/releases/download/1.2.0/jrctl-1.2.0-darwin.tar.gz"
+	sha256 "a5f2c450dd1d1504575072c4f2cda16f085d0312fb675842eb5ceddc94d54546"
+	version "1.2.0"
 
 	def install
-		mkdir_p "/etc/jrctl"
 		bin.install "bin/jrctl_darwin_amd64" => "jrctl"
 		man1.install "man/jrctl-firewall-allow.1"
 		man1.install "man/jrctl-firewall-list.1"
@@ -22,11 +19,19 @@ class Jrctl < Formula
 		man1.install "man/jrctl-server-restart.1"
 		man1.install "man/jrctl-server-version.1"
 		man1.install "man/jrctl-server.1"
+		man1.install "man/jrctl-transfer-receive.1"
+		man1.install "man/jrctl-transfer-send.1"
+		man1.install "man/jrctl-transfer.1"
 		man1.install "man/jrctl.1"
 	end
 
 	test do
 		system "#{bin}/jrctl", "-v"
+	end
+
+	def caveats
+		"Bash completions are available but were not installed. For more info see:\n\n" +
+		"    $ jrctl completion --help\n\n"
 	end
 
 end
